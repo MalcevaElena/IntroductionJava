2aba140356e9d84aef4a8ec4708bd9c8c85ca5f9
Malceva Elena <malceva.em@mail.ru>
2021-02-14 01:26:51 +0700

    Commit7 Modul2

---
+++ src/Modul2/ArraysOfArrays/Task14.java
@@ -0,0 +1,33 @@
+/*Сформировать случайную матрицу m x n, состоящую из нулей и единиц, причем в каждом столбце число
+единиц равно номеру столбца.
+ */
+package Modul2.ArraysOfArrays;
+
+public class Task14 {
+    public static void main(String[] args) {
+        int m = 7;
+        int n = 5;
+        int [][] matrix = new int [m][n];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = 0;
+                if (i < matrix[i].length && i<=j) {
+                    for (int h = i; h >= 0; h--) {
+                        matrix[h][j] = 1;
+                    }
+                }
+            }
+        }
+        System.out.println("Матрица "+m+ " x "+n+":");
+        printMatrix(matrix);
+    }
+
+    public static void printMatrix (int [][] arrays){
+        for (int i = 0; i < arrays.length; i++) {
+            for (int j = 0; j < arrays[i].length; j++) {
+                System.out.printf("%3d\t", arrays[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
