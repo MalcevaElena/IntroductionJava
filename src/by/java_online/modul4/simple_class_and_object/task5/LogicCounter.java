89c10bca67d5097bfdcf7723fe0c8f89245ab074
Malceva Elena <malceva.em@mail.ru>
2021-06-03 00:39:18 +0700

    Модуль 4 Простейшие классы и объекты

---
+++ src/by/java_online/modul4/simple_class_and_object/task5/LogicCounter.java
@@ -0,0 +1,21 @@
+package by.java_online.modul4.simple_class_and_object.task5;
+
+public class LogicCounter {
+
+    public static int increment(int number, int max) {
+        if (number == max) {
+            return number;
+        } else {
+            return ++number;
+        }
+    }
+
+    public static int decrement(int number, int min) {
+            if (number==min){
+                return number;
+            }
+            else {
+                return --number;
+            }
+        }
+}
